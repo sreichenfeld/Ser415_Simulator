b478bfa1b5d55f52585f0dc24bf4bc8c1149bc12
elykauf <emkaufm1@asu.edu>
2019-11-13 16:10:02 -0700

    added lights

---
+++ ser415/SER415/Assets/Scripts/LightCycle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightCycle : MonoBehaviour
+{
+    public GameObject lights;
+    public LightCycle instance;
+    SpriteRenderer m_SpriteRenderer;
+    int state = 0;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_SpriteRenderer.color = Color.blue;
+        if (this.instance == null)
+            {
+                this.instance = new LightCycle();
+                this.state = 0;
+            }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            this.timer = 0;
+            this.state++;
+            if (this.state > 2)
+            {
+                this.state = 0;
+            }
+            if (this.state == 0)
+            {
+                m_SpriteRenderer.color = Color.green;
+            } else if(this.state == 1)
+            {
+                m_SpriteRenderer.color = Color.red;
+            } else if( this.state == 2)
+            {
+                m_SpriteRenderer.color = Color.blue;
+            }
+        }
+    }
+}
