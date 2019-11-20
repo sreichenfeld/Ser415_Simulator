693294391e41018a80ac82c0d6a6c1b175ab67c3
elykauf <emkaufm1@asu.edu>
2019-11-20 12:01:27 -0700

    dont know what I changed

--- ser415/SER415/Assets/Scripts/LightCycle.cs
+++ ser415/SER415/Assets/Scripts/LightCycle.cs
@@ -8,13 +8,14 @@ public class LightCycle : MonoBehaviour
     public GameObject lights;
     public LightCycle instance;
     SpriteRenderer m_SpriteRenderer;
-    int state = 0;
+    public int state = 0;
+    static Color[] cycles = new Color[] { Color.green, Color.yellow, Color.red };
     float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
-        m_SpriteRenderer.color = Color.blue;
+        m_SpriteRenderer.color = Color.green;
         if (this.instance == null)
             {
                 this.instance = new LightCycle();
@@ -23,6 +24,7 @@ public class LightCycle : MonoBehaviour
         
     }
 
+    
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -37,13 +39,13 @@ public class LightCycle : MonoBehaviour
             }
             if (this.state == 0)
             {
-                m_SpriteRenderer.color = Color.green;
+                m_SpriteRenderer.color = cycles[this.state];
             } else if(this.state == 1)
             {
-                m_SpriteRenderer.color = Color.red;
+                m_SpriteRenderer.color = cycles[this.state];
             } else if( this.state == 2)
             {
-                m_SpriteRenderer.color = Color.blue;
+                m_SpriteRenderer.color = cycles[this.state];
             }
         }
     }
